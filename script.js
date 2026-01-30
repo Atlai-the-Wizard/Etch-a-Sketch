5884a91c3f715d8439fde237bb2b71c04213fd4d
Atlai <atlaithewizard@gmail.com>
2026-01-30 15:57:43 +0900

    Add event listener and function to change cell color

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.getElementById('container');
+const newGrid = document.getElementById('new-grid');
+const clearGrid = document.getElementById('clear-grid');
 
 function makeCells(numCells) {
   for (let i = 0; i < numCells * numCells; i++) {
@@ -6,9 +8,20 @@ function makeCells(numCells) {
     cell.classList.add('cells');
     cell.style.width = 100 / numCells + `%`;
     // add mouseover event function
+    cell.addEventListener('mouseover', (e) => {
+      e.target.style.backgroundColor = 'black';
+    });
     container.appendChild(cell);
   }
 }
+
+// function that makes the hover event listener for the cells
+
+// function that creates a new grid of cells
+
+// function that clears grid
+
+
 
 
 
