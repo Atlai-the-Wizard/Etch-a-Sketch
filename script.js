e12a92af95dc6b4e369069702d8ebed2de2334ce
Atlai <atlaithewizard@gmail.com>
2026-01-30 11:19:10 +0900

    Rewrite code for cleanliness and proper function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const container = document.querySelector('.container');
+const container = document.getElementById('container');
 
 function makeCells(numCells) {
   for (let i = 0; i < numCells; i++) {
-    let cells = document.createElement('div');
-    cells.classList.add('cells')
-    container.appendChild(cells);
+    const cell = document.createElement('div');
+    cell.classList.add('cells');
+    // add mouseover event function
+    container.appendChild(cell);
   }
 }
 
-makeCells(100);
+// run makeCells function
+
+// const container = document.querySelector('.container');
+
+// function makeCells(numCells) {
+//   for (let i = 0; i < numCells; i++) {
+//     let cells = document.createElement('div');
+//     cells.classList.add('cells')
+//     container.appendChild(cells);
+//   }
+// }
+
+// makeCells(32);
