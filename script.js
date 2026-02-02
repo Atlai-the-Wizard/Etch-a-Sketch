aa15222c05e0349a4b468dcf15ee4658be8444fa
Atlai <atlaithewizard@gmail.com>
2026-02-02 12:03:08 +0900

    Add buttons to reset grid and create new grid
    Add function to reset current grid to white
    
    Add function to clear grid
    
    Add function to make new grid from prompt
    
    Add button to reset current grid to white
    
    Add button to make new grid from prompt

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 const container = document.getElementById('container');
-const newGrid = document.getElementById('new-grid');
-const clearGrid = document.getElementById('clear-grid');
+const newGridButton = document.getElementById('new-grid');
+const clearGridButton = document.getElementById('clear-grid');
+let cellCount = 0;
 
+// function to make cells
 function makeCells(numCells) {
   for (let i = 0; i < numCells * numCells; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cells');
     cell.style.width = 100 / numCells + `%`;
-    // add mouseover event function
     cell.addEventListener('mouseover', (e) => {
       e.target.style.backgroundColor = 'black';
     });
     container.appendChild(cell);
   }
+  cellCount = numCells;
 }
 
-// function that makes the hover event listener for the cells
+// function that clears grid
+function clearGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
 
 // function that creates a new grid of cells
+function newGrid() {
+  let newCellGrid = prompt('Enter a number between 1 and 100:');
+  newCellGrid = parseInt(newCellGrid);
+  if (newCellGrid >= 1 && newCellGrid <= 100) {
+    makeCells(newCellGrid);
+  } else {
+    alert('Enter a number between 1 and 100');
+  }
+}
 
-// function that clears grid
+// function that resets current grid to white
+clearGridButton.addEventListener('click', () => {
+  clearGrid();
+  makeCells(cellCount);
+})
 
-
-
-
+// button that creates new grid
+newGridButton.addEventListener('click', () => {
+  clearGrid();
+  newGrid();
+})
 
 makeCells(16);
